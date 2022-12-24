fd9e84aec043115b249860cc51334bd8ba39a64b
OneProjack <118982049+OneProjack@users.noreply.github.com>
2022-12-24 12:08:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5029255799 2013745499").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5029255799 2013745499 5042563853").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
